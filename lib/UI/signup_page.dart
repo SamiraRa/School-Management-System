a3813e6169208df8d8555e721d5b29b043dd192e
Samira Rashid <samirarashid698@gmail.com>
2025-05-19 16:27:34 +0600

    sdk changed due to issues:

--- lib/UI/signup_page.dart
+++ lib/UI/signup_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:school_management_application/UI/homepage_screen.dart';
 import 'package:school_management_application/UI/loginpage.dart';
 
 class SignupPage extends StatefulWidget {
